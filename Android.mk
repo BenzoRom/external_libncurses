7e2de413b83b82a000a246ebd76d42d6cd7a14f7
mydongistiny <jaysonedson@gmail.com>
2019-07-05 15:22:42 -0700

    libncurses: Convert to Android.bp
    Change-Id: Ieb09c3dd3570b183e0948c0ddaa7cb4dd58a6976
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,38 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-c-files-under, ncurses/tty) 
-LOCAL_SRC_FILES += $(call all-c-files-under, ncurses/base) 
-LOCAL_SRC_FILES := $(filter-out ncurses/base/lib_driver.c ncurses/base/sigaction.c, $(LOCAL_SRC_FILES))
-LOCAL_SRC_FILES += $(call all-c-files-under, ncurses/tinfo) 
-LOCAL_SRC_FILES := $(filter-out ncurses/tinfo/doalloc.c ncurses/tinfo/make_keys.c ncurses/tinfo/tinfo_driver.c, $(LOCAL_SRC_FILES))
-
-LOCAL_SRC_FILES += ncurses/trace/lib_trace.c \
-		ncurses/trace/varargs.c \
-		ncurses/trace/visbuf.c \
-		ncurses/codes.c \
-		ncurses/comp_captab.c \
-		ncurses/expanded.c \
-		ncurses/fallback.c \
-		ncurses/lib_gen.c \
-		ncurses/lib_keyname.c \
-		ncurses/names.c \
-		ncurses/unctrl.c \
-
-LOCAL_SRC_FILES := $(sort $(LOCAL_SRC_FILES))
-		
-LOCAL_CFLAGS := -DHAVE_CONFIG_H -U_XOPEN_SOURCE -D_XOPEN_SOURCE=500 -U_POSIX_C_SOURCE -D_POSIX_C_SOURCE=199506L -DNDEBUG 
-LOCAL_CFLAGS += -Wno-unused-parameter
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-		$(LOCAL_PATH)/include \
-		$(LOCAL_PATH)/ncurses \
-
-LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE := libncurses
-
-include $(BUILD_SHARED_LIBRARY)
-
 # Copy only basic terminal type definitions as ncurses-base in debian does by default.
 # http://anonscm.debian.org/gitweb/?p=collab-maint/ncurses.git;a=blob;f=debian/rules;hb=HEAD#l140
 TERMINFO_FILES := \
